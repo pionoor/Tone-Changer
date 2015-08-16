b3dbf9663011be1e770b8ca523dd0dfb850e9905
Noor Thabit <pionoor@gmail.com>
2015-08-15 23:28:15 -0500

    Initial Commit

---
+++ Tone Changer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tone Changer
+//
+//  Created by Noor Thabit on 8/15/15.
+//  Copyright (c) 2015 Noor Thabit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
